2aecef147eea0727132e1ea55ef792e3653de5d7
luanhroliveira <54966069+luanhroliveira@users.noreply.github.com>
2020-03-18 13:19:25 -0300

    Update Program.java

--- src/br/com/java/app/Program.java
+++ src/br/com/java/app/Program.java
@@ -42,106 +42,103 @@ public class Program {
 		}
 		
 		
-			for (int i = 1; i <= n; i++) {
-				try {
-					System.out.println("Enter #" + i + " employee data:");
-					System.out.print("Name: ");
-					String name = sc.nextLine();
-		
-					System.out.print("CPF: ");
-					String cpf = sc.nextLine();
-					
-					int idBadge = 0;
-					while(idBadge == 0) {
-						try {
-							System.out.print("Badge: ");
-							 idBadge = sc.nextInt();
-						
-						}catch(InputMismatchException e) {
-							System.out.println("invalid entry, try again.\n ");
-						}
-						sc.nextLine();
-					}
-					
-					double salary = 0;
-					while(salary == 0) {
-						try {
-							System.out.print("Salary: ");
-							salary = sc.nextDouble();
-								
-						}catch(InputMismatchException e) {
-							System.out.println("invalid entry, try again.\n ");
-						}
-						sc.nextLine();
-					}
-			
-					System.out.println();
-					System.out.println("Enter department data: ");
-					
-					Department department = null;
-					while(department == null) {
-						try {
-							System.out.print("Department: ");
-							department = Department.valueOf(sc.nextLine());
-								
-						}catch(IllegalArgumentException e) {
-							System.out.println("invalid entry, try again.\n ");
-						}
-					}
+		for (int i = 1; i <= n; i++) {
+			try {
+				System.out.println("Enter #" + i + " employee data:");
+				System.out.print("Name: ");
+				String name = sc.nextLine();
 
-		
-					System.out.println();
-					System.out.println("Enter data for calculation: ");
-					System.out.print("Hours performed (HH:mm): ");
-					String hoursPerformed = sc.nextLine();
-				
-					double dsrPercentage = 0;
-					while(dsrPercentage == 0) {
-						try {
-							System.out.print("current dsr percentage: ");
-							dsrPercentage = sc.nextDouble();
-								
-						}catch(InputMismatchException e) {
-							System.out.println("invalid entry, try again.\n ");
-						}
-						sc.nextLine();
-					}
+				System.out.print("CPF: ");
+				String cpf = sc.nextLine();
 
-					System.out.println("Personal assistence(y/n)? ");
-					char x = sc.next().charAt(0);
-			
-					
-					if(x == 'y') {
-						int travels = 0;
-						while(travels == 0) {
-							try {
-								System.out.print("Travels: ");
-								travels = sc.nextInt();	
-							
-							}catch(InputMismatchException e) {
-								System.out.println("invalid entry, try again.\n ");
-							}
-							sc.nextLine();
-						}
-		
-						ExtraHours personal = new PersonalAssistence(hoursPerformed, dsrPercentage, salary, travels);
-						
-						employee = new Employee(new Date(), name, idBadge, cpf, salary, department, personal);
-					}else {
-						ExtraHours remote = new RemoteAssistence(hoursPerformed, dsrPercentage, salary);
-		
-						employee = new Employee(new Date(), name, idBadge, cpf, salary, department, remote);
-					}
-					employeeList.add(employee);
-					System.out.println();
-					
-					employee.workersList(employeeList);
-					
+				int idBadge = 0;
+				while(idBadge == 0) {
+					try {
+						System.out.print("Badge: ");
+						 idBadge = sc.nextInt();
+
 					}catch(InputMismatchException e) {
-						System.out.println("Entrada inválida, tente novamente.");
+						System.out.println("invalid entry, try again.\n ");
+					}
+					sc.nextLine();
+				}
+
+				double salary = 0;
+				while(salary == 0) {
+					try {
+						System.out.print("Salary: ");
+						salary = sc.nextDouble();
+
+					}catch(InputMismatchException e) {
+						System.out.println("invalid entry, try again.\n ");
+					}
+					sc.nextLine();
+				}
+
+				System.out.println();
+				System.out.println("Enter department data: ");
+
+				Department department = null;
+				while(department == null) {
+					try {
+						System.out.print("Department: ");
+						department = Department.valueOf(sc.nextLine());
+
+					}catch(IllegalArgumentException e) {
+						System.out.println("invalid entry, try again.\n ");
 					}
 				}
-			
-				sc.close();
+
+				System.out.println();
+				System.out.println("Enter data for calculation: ");
+				System.out.print("Hours performed (HH:mm): ");
+				String hoursPerformed = sc.nextLine();
+
+				double dsrPercentage = 0;
+				while(dsrPercentage == 0) {
+					try {
+						System.out.print("current dsr percentage: ");
+						dsrPercentage = sc.nextDouble();
+
+					}catch(InputMismatchException e) {
+						System.out.println("invalid entry, try again.\n ");
+					}
+					sc.nextLine();
+				}
+
+				System.out.println("Personal assistence(y/n)? ");
+				char x = sc.next().charAt(0);
+
+				if(x == 'y') {
+					int travels = 0;
+					while(travels == 0) {
+						try {
+							System.out.print("Travels: ");
+							travels = sc.nextInt();	
+
+						}catch(InputMismatchException e) {
+							System.out.println("invalid entry, try again.\n ");
+						}
+						sc.nextLine();
+					}
+
+					ExtraHours personal = new PersonalAssistence(hoursPerformed, dsrPercentage, salary, travels);
+
+					employee = new Employee(new Date(), name, idBadge, cpf, salary, department, personal);
+				}else {
+					ExtraHours remote = new RemoteAssistence(hoursPerformed, dsrPercentage, salary);
+
+					employee = new Employee(new Date(), name, idBadge, cpf, salary, department, remote);
+				}
+				employeeList.add(employee);
+				System.out.println();
+
+				employee.workersList(employeeList);
+
+				}catch(InputMismatchException e) {
+					System.out.println("Entrada invÃ¡lida, tente novamente.");
+				}
+			}
+		sc.close();
 	}
 }
